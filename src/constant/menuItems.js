6576cacc2a29c055e0fbef69524b87d68a666671
farruop <rafayKhanpvt@gmail.com>
2025-02-03 22:49:18 +0530

    Add: constant components for better code structure

---
+++ src/constant/menuItems.js
@@ -0,0 +1,8 @@
+export const menuItems = [
+    { name: "Our Menu", link: "/ourmenu", type: "link" },
+    { name: "Special Offers", link: "/specialoffers", type: "link" },
+    { name: "About", link: "/about", type: "link" },
+    { name: "Reviews", link: "/reviews", type: "link" },
+    {name:"contact Us", link:"/contact", type: "link"},
+    { name: "Order", link: "/order", type: "button" }
+  ];
