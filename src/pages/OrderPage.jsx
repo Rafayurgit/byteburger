3638db972ad60b9c0e6afd7031d91bd0b1bc0499
farruop <rafayKhanpvt@gmail.com>
2025-02-08 19:05:29 +0530

    Add: update orderpage and style

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import OrderOptions from "../components/OrderOptions";
 import Section from "../components/Section";
+import Footer from "../components/Footer"
 
 const platforms = [
   { name: "Zomato", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Zomato_logo.png/600px-Zomato_logo.png", link: "https://link.zomato.com/xqzv/rshare?id=979507972a1b9bf8" },
@@ -25,6 +26,13 @@ const OrderPage = () => (
     <Section id="direct-order" title="Order Directly from Us">
       <p>Want fresh burgers directly from us? Click below!</p>
       <a href="/direct-order" className="call-to-action">Order Now</a>
+    </Section>
+
+    <Section>
+        <img src="https://images.unsplash.com/photo-1517601278517-456741619dad?q=80&w=1812&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+        alt="Delicious Burger" 
+        className="order-image" 
+        />
     </Section>
   </div>
 );
