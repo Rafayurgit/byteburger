560201561f8f0de103d5d5fa42c476c38b3136d8
farruop <rafayKhanpvt@gmail.com>
2025-02-13 23:21:05 +0530

    Add: footer redirect link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => (
-    <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} Byte Burger. All rights reserved.</p>
-      <p>
-        <a href="#about">About</a> | <a href="#contact">Contact</a>
-      </p>
-    </footer>
-  );
-  
-  export default Footer;
-  
+  <footer className="footer">
+    <p>&copy; {new Date().getFullYear()} Byte Burger. All rights reserved.</p>
+    <p>
+      <Link to="/about">About</Link> | <Link to="/contact">Contact</Link>
+    </p>
+  </footer>
+);
+
+export default Footer;
