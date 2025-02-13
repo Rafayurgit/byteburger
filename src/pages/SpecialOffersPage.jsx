235c05ae72c735e6ccb7cf6505882c791500c542
farruop <rafayKhanpvt@gmail.com>
2025-02-13 23:31:16 +0530

    Add: update style for special offer page

--- src/pages/SpecialOffersPage.jsx
+++ src/pages/SpecialOffersPage.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const SpecialOffersPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center p-8">
-      <h1 className="text-5xl font-extrabold text-orange-500 mb-4">Special Offers</h1>
-      <p className="text-2xl text-gray-700 font-semibold">
+    <div className="flex flex-col items-center justify-center min-h-screen text-center p-8">
+      <h1 className="text-5xl font-extrabold text-orange-500 mb-4 text-shadow-lg ">Special Offers</h1>
+      <p className="text-2xl text-gray-700 font-semibold p-2 text-shadow-md">
         Visit our shop or check the notice board for exciting special offers! 🎉🔥
       </p>
     </div>
