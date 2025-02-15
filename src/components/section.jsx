8466d21a3b237a9b7cdb6396a41b4e11c0494569
farruop <rafayKhanpvt@gmail.com>
2025-02-15 22:16:11 +0530

    Add: add claddName functionality to section component

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Section = ({ id, title, children }) => (
-    <section id={id} className="section">
-      {title && <h2>{title}</h2>}
+const Section = ({ className, id, title, children }) => {
+  return (
+    <section className={`my-4 p-4 ${className}`} id={id}>
+      <h2 className="text-2xl font-bold">{title}</h2>
       {children}
     </section>
   );
-  
-  export default Section;
-  
+};
+
+export default Section;
