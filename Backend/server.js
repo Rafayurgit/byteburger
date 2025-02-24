c21c4668039e42e2c807476b02f886bbcb6a27f7
farruop <rafayKhanpvt@gmail.com>
2025-02-24 22:57:23 +0530

    Getting started with backend

--- Backend/server.js
+++ Backend/server.js
@@ -3,4 +3,5 @@ import express from "express";
 const app= express();
 const PORT = 8080 || process.env.port;
 app.listen(PORT, ()=>{
-    console.log(`Server is listini
+    console.log(`Server is listining on ${PORT}`);
+})
