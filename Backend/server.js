e119471e44d2132d180c4122c6dbe43451a2eb76
farruop <rafayKhanpvt@gmail.com>
2025-02-27 23:52:46 +0530

    Add: mongoose,dotenv,cors dependencies

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,32 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app= express();
-const PORT = 8080 || process.env.port;
+
+app.use(express.json());
+app.use(cors());
+
+// mongoose.connect(process.env.MONGO_URL, {
+//     useNewParse:true,
+//     useUnifiedTopology:true
+// })
+// .then(()=> console.log("MongoDb connected"))
+// .catch((error)=>console.log("MongoDb connection Error", error));
+
+
+app.get("/", (req, res)=>{
+    res.send("Welcome to byteBurger api");
+});
+
+const PORT = 8080 || process.env.PORT;
+
 app.listen(PORT, ()=>{
     console.log(`Server is listining on ${PORT}`);
 })
+
+
+
