536f4a09d89c17fbb232b086477e750bcd17b1c5
farruop <rafayKhanpvt@gmail.com>
2025-03-01 01:08:37 +0530

    Update structure

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,16 @@
 import express from "express";
-import mongoose from "mongoose";
+// import connectDb from "./src/config/db";
 import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
+// connectDb();
 
 const app= express();
-
 app.use(express.json());
 app.use(cors());
 
-// mongoose.connect(process.env.MONGO_URL, {
-//     useNewParse:true,
-//     useUnifiedTopology:true
-// })
-// .then(()=> console.log("MongoDb connected"))
-// .catch((error)=>console.log("MongoDb connection Error", error));
+
 
 
 app.get("/", (req, res)=>{
