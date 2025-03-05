2adaed141239ffc35370170b2fc697901e64e2ce
farruop <rafayKhanpvt@gmail.com>
2025-03-06 00:01:00 +0530

    Add: /login endpoint in authRoutes

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -20,3 +20,24 @@ router.post("/register" , async(req,res)=>{
         res.status(500).json({error: error.message})
     }
 })
+
+router.post("/login", async(req,res)=>{
+    try {
+        const {email, password}= req.body;
+        const user= await User.findOne({email});
+
+        if(!user) return res.status(400).json({message:"Invalid credentials"})
+        
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({message:"Incalid credentials"});
+
+        const token = jwt.sign({userId : user._id}, process.env.JWT_SECRET, {expiresIn: "24h"});
+        res.json({token})
+
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
+
+export default router;
