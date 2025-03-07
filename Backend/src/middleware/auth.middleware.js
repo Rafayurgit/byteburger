c342271e38024c88368a0c55978ae018d9fca9f5
farruop <rafayKhanpvt@gmail.com>
2025-03-08 00:36:54 +0530

    Add: adminMiddleware to authMiddleware

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -1,15 +1,17 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.model";
 
-
-const authMiddleware=(req,res,next)=>{
-
-    const token = req.header("authorization");
-
-    if(!token) return res.status(401).json({message:"Access denied, no token available"})
+const authMiddleware= async(req,res,next)=>{
 
     try {
-        const decode = jwt.verify(token, process.env.JWT_SECRET);
-        req.user= decode;
+
+        const token = req.header("authorization");
+        if(!token) return res.status(401).json({message:"Access denied, no token available"})
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user= await User.findById(decoded.userId).select("-password");
+
+        if(!req.user) return res.status(401).json({message:"User not found"})
         next();
 
     } catch (error) {
@@ -18,4 +20,9 @@ const authMiddleware=(req,res,next)=>{
 
 };
 
-export default authMiddleware;
+const adminMiddleware =(req,res,next)=>{
+    if(req.user.role !== "admin") return res.status(403).json({message:"Access denied. Admin only"})
+    next();
+}
+
+export default {authMiddleware, adminMiddleware};
