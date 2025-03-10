fd004e942f8b0be7b44cd6571e4be8d827391002
farruop <rafayKhanpvt@gmail.com>
2025-03-11 01:58:08 +0530

    Add: /:id deletebyId route to reviews.route endpoint

--- Backend/src/routes/reviews.routes.js
+++ Backend/src/routes/reviews.routes.js
@@ -25,3 +25,15 @@ router.post("/", async(req,res)=>{
         res.status(500).json({message: error.message})
     }
 })
+
+router.delete("/:id", async(req,res)=>{
+    try {
+        const id= req.params.id;
+        const deletedReview= await Review.findByIdAndDelete(id);
+        if(!deletedReview) return res.status(404).json({message:"Review not found"})
+
+        res.json({message: "Review delted successfully"})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
